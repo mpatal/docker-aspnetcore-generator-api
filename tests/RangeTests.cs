c3217ade86ca0745cae308a25b8724431ebd28ed
Marvin Patal <marvin.patal@hotmail.com>
2017-11-02 22:25:59 -0400

    Add fail test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -26,7 +26,7 @@ namespace tests
             var counter =0;
             var generated = range.Of(()=> values[counter++]);
 
-            Assert.Equal(new [] {"a", "b", "c"}, generated.ToArray());
+            Assert.Equal(new [] {"a", "c", "b"}, generated.ToArray());
         }
     }
 }
