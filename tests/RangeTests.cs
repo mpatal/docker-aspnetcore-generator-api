645d604e2b5831583ae2337f8be2bbce2498be92
Marvin Patal <marvin.patal@hotmail.com>
2017-11-02 22:30:02 -0400

    Add fail test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -19,6 +19,7 @@ namespace tests
 
         }
 
+        [Fact]
         public void SortShouldOrderResults()
         {
             var range = new Range { Count = 3, Sort = true};
@@ -26,7 +27,7 @@ namespace tests
             var counter =0;
             var generated = range.Of(()=> values[counter++]);
 
-            Assert.Equal(new [] {"a", "c", "b"}, generated.ToArray());
+            Assert.Equal(new [] {"a", "b", "c"}, generated.ToArray());
         }
     }
 }
